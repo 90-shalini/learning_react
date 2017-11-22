d64cf7ad158e83b7402320226361242823a717f5
EU\sarora <sarora@planview.com>
2017-11-22 10:07:09 +0530

    files updated

--- hack_a_week/components/filter.js
+++ hack_a_week/components/filter.js
@@ -1,19 +1,21 @@
 class FilterMenu extends React.Component {
    constructor() {
      super();
-     this.state={open: true, envs: [], db: [], selectedEnvironment: ''};
+     this.state={open: true,
+      env_list: [],
+      db_list: [],
+      selectedEnvironment: ''};
    }
+
    handleChange = (event) => {
-    this.setState({selectedEnvironment : event.target.value});
-    console.log(this.state.selectedEnvironment)
+    this.setState(
+                {selectedEnvironment : event.target.value}
+                );
     var db_list_url = 'http://'+window.location.hostname+':5000/ui/'+ this.state.selectedEnvironment;
-    var db_element = document.getElementById('db_dropdown').options;
     axios.get(db_list_url)
             .then(response => {
                 console.log(response.data);
-                response.data.forEach((option) => this.state.db.add(new Option(
-                option.pve_dsn)));
-                db_element.selected(response.data[0])
+                this.setState({db_list: response.data})
              })
             .catch(function (error){
                 console.log(error)
@@ -22,11 +24,10 @@ class FilterMenu extends React.Component {
    }
    componentDidMount(){
     var env_list_url = 'http://'+window.location.hostname+':5000/ui';
-    var env_element = document.getElementById('env_dropdown').options;
     axios.get(env_list_url)
             .then(response => {
                 console.log(response.data);
-                this.setState({envs: this.state.envs.concat(response.data)})
+                this.setState({env_list: response.data})
              })
             .catch(function (error){
                 console.log(error)
@@ -36,42 +37,40 @@ class FilterMenu extends React.Component {
       var Panel = ReactBootstrap.Panel;
       var Button = ReactBootstrap.Button;
       var Glyphicon = ReactBootstrap.Glyphicon;
-     return (
-         <div id="accordion" class="panel panel-lg panel-custom">
-          <Button onClick={() => this.setState({ open: !this.state.open })}>
-                 <Glyphicon bsStyle="Info" className="glyphicon" glyph="filter"/>
-          </Button>
-          <Panel collapsible defaultExpanded expanded={this.state.open}>
-            <div>
-                <Env_dropdown envs={this.state.envs} onChangeSelection ={this.handleChange}/>
-                <DB_dropdown db={this.state.db}/>
-            </div>
-          </Panel>
-         </div>
-     );
+         return (
+             <div id="accordion" class="panel panel-lg panel-custom">
+              <Button onClick={() => this.setState({ open: !this.state.open })}>
+                     <Glyphicon bsStyle="Info" className="glyphicon" glyph="filter"/>
+              </Button>
+              <Panel collapsible defaultExpanded expanded={this.state.open}>
+                <div>
+                    <Env_dropdown envs={this.state.env_list} onChangeSelection={this.handleChange}/>
+                    <DB_dropdown db={this.state.db_list}/>
+                </div>
+              </Panel>
+             </div>
+         );
    }
 }
 
 class Env_dropdown extends React.Component {
     constructor(props){
         super(props);
-        console.log(this.props);
+        console.log(this.props)
     };
     render(){
-        <div className="ProductFilter__section-container">
+         var envOptions = this.props.envs.map((env) =>{
+            return <option>{env.pve_env}</option>
+         })
+        return(<div className="ProductFilter__section-container">
                    <label>Environment</label>
-                   <select id="env_dropdown" onChange={(event) => this.props.onChangeFunction(event)}>
-                   {this.props.envs.map(env => <Option key={env.name}{...env}/>
-                                )}
+                   <select id="env_dropdown" onChange={(event) => this.props.onChangeSelection(event)}>
+                       {envOptions}
                    </select>
                 </div>
+           );
+        }
     }
-}
-
-const Option = (props) => {
-        return ( <option>{props.name}</option>
-        );
-    };
 
 class DB_dropdown extends React.Component {
     constructor(props){
@@ -79,13 +78,15 @@ class DB_dropdown extends React.Component {
         console.log(this.props);
     };
     render(){
-        <div className="ProductFilter__section-container">
+        var dbOptions = this.props.db.map((db_name) =>{
+            return <option>{db_name.pve_dsn}</option>
+         })
+        return (<div className="ProductFilter__section-container">
                    <label>Database</label>
-                   <select id="db_dropdown" onChange={(event) => this.props.onChangeFunction(event)}>
-                   {this.props.db.map(db_name => <Option key={db_name.pve_dsn}{...db_name}/>
-                                )}
+                   <select id="db_dropdown">
+                        {dbOptions}
                    </select>
-                </div>
+                </div>);
     }
 }
-ReactDOM.render(<FilterMenu/>, document.getElementById('filter'));
+    ReactDOM.render(<FilterMenu/>, document.getElementById('filter'));
