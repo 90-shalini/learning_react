f91beb66636964a014bbaa1b397efb2502a5d22e
EU\sarora <sarora@planview.com>
2017-11-22 13:44:38 +0530

    filter files updated

--- hack_a_week/components/filter.js
+++ hack_a_week/components/filter.js
@@ -4,23 +4,39 @@ class FilterMenu extends React.Component {
      this.state={open: true,
       env_list: [],
       db_list: [],
-      selectedEnvironment: ''};
+      selectedEnvironment: 'Select Environment:',
+      selectedDatabase: 'Select Database:',
+      result_list:[]};
    }
 
-   handleChange = (event) => {
-    this.setState(
-                {selectedEnvironment : event.target.value}
-                );
-    var db_list_url = 'http://'+window.location.hostname+':5000/ui/'+ this.state.selectedEnvironment;
-    axios.get(db_list_url)
-            .then(response => {
-                console.log(response.data);
-                this.setState({db_list: response.data})
-             })
-            .catch(function (error){
-                console.log(error)
-            });
+   handleEnvChange = (event) => {
+   this.setState({selectedEnvironment: event.target.value}, () => {
+        var db_list_url = 'http://'+window.location.hostname+':5000/ui/'+ this.state.selectedEnvironment;
+        axios.get(db_list_url)
+                .then(response => {
+                    console.log(response.data);
+                    this.setState({db_list: response.data})
+                 })
+                .catch(function (error){
+                    console.log(error)
+                });
+    });
+   }
+   handleDbChange = (event) => {
+   this.setState({selectedDatabase : event.target.value}, () =>{
+   var results_url = 'http://'+window.location.hostname+':5000/ui/tests/' + this.state.selectedEnvironment +'/'+
+                        this.state.selectedDatabase;
+   console.log(results_url)
+   axios.get(results_url)
+        .then(response => {
+            console.log(response.data);
+            this.setState({result_list: response.data})
+         })
+        .catch(function (error){
+            console.log(error)
+        });
 
+   });
    }
    componentDidMount(){
     var env_list_url = 'http://'+window.location.hostname+':5000/ui';
@@ -32,7 +48,8 @@ class FilterMenu extends React.Component {
             .catch(function (error){
                 console.log(error)
             });
-   }
+    }
+
    render() {
       var Panel = ReactBootstrap.Panel;
       var Button = ReactBootstrap.Button;
@@ -44,8 +61,9 @@ class FilterMenu extends React.Component {
               </Button>
               <Panel collapsible defaultExpanded expanded={this.state.open}>
                 <div>
-                    <Env_dropdown envs={this.state.env_list} onChangeSelection={this.handleChange}/>
-                    <DB_dropdown db={this.state.db_list}/>
+                    <Env_dropdown envs={this.state.env_list} onChangeSelection={this.handleEnvChange}/>
+                    <DB_dropdown db={this.state.db_list} onChangeSelection={this.handleDbChange}/>
+                    <Results_table results={this.state.result_list}/>
                 </div>
               </Panel>
              </div>
@@ -56,11 +74,11 @@ class FilterMenu extends React.Component {
 class Env_dropdown extends React.Component {
     constructor(props){
         super(props);
-        console.log(this.props)
+        console.log(this.props);
     };
     render(){
          var envOptions = this.props.envs.map((env) =>{
-            return <option>{env.pve_env}</option>
+            return <option>{env}</option>
          })
         return(<div className="ProductFilter__section-container">
                    <label>Environment</label>
@@ -79,14 +97,43 @@ class DB_dropdown extends React.Component {
     };
     render(){
         var dbOptions = this.props.db.map((db_name) =>{
-            return <option>{db_name.pve_dsn}</option>
+            return <option>{db_name}</option>
          })
         return (<div className="ProductFilter__section-container">
                    <label>Database</label>
-                   <select id="db_dropdown">
+                   <select id="db_dropdown" onChange={(event) => this.props.onChangeSelection(event)}>
                         {dbOptions}
                    </select>
                 </div>);
     }
 }
+
+class Results_table extends React.Component {
+            constructor(props) {
+                super(props);
+                console.log('In results table')
+                this.state = {
+                    results: [props.results]
+                };
+                console.log(this.state.results)
+            }
+            render() {
+       let Table = ReactBootstrap.Table;
+       var resultsRow = this.props.results.map((result_row) => {
+            return
+       })
+       return (
+           <div id="column_data">
+               <Table ref='table1' className="striped bordered condensed hover">
+                    <TableHeaderColumn dataField='zid' isKey={ true } dataSort={ true }>Zephyr ID</TableHeaderColumn>
+                    <TableHeaderColumn dataField='descr'>Description</TableHeaderColumn>
+                    <TableHeaderColumn dataField='status'>Test Status</TableHeaderColumn>
+                    <TableHeaderColumn dataField='last_run'>Last Run</TableHeaderColumn>
+               </Table>
+            </div>
+        );
+    }
+        }
+
     ReactDOM.render(<FilterMenu/>, document.getElementById('filter'));
+
